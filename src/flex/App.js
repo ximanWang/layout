20b85eed5b9e93923d422f91c42e653528ed1a82
ximan <ximan.wxm@gmail.com>
2019-06-13 15:05:59 +1000

    feat(add pages):

--- src/flex/App.js
+++ src/flex/App.js
@@ -1,57 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-// import './App.css';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <div className="header"> 
-      <div className="header-title"> ClearView</div>
+      <div className="header-title"> Index</div>
       </div>
 
       <div className="main">
         <div className="left-side">
-          <div className="profile">Full Financial Profile</div>
+          <div className="profile">Full Profile</div>
           <div className="list">
             <div className="list-item">
               <span className="tag"></span>
-              <div className="title">Personal information</div>
+              <div className="title">List item 1</div>
             </div>
             <div className="list-item">
               <span className="tag"></span>
-              <div className="title">Assets & Liabilities</div>
+              <div className="title">List Item 2</div>
             </div>
             <div className="list-item">
               <span className="tag"></span>
-              <div className="title">planning for the future</div>
+              <div className="title">List item 3</div>
               <div className="list-item-list">
-                <div className="list-item-list-item">Goals</div>
-                <div className="list-item-list-item">Retirement planning</div>
-                <div className="list-item-list-item">Estate planning</div>
+                <div className="list-item-list-item">Inner item 1</div>
+                <div className="list-item-list-item">Inner item 2</div>
+                <div className="list-item-list-item">Inner item 3</div>
               </div>
             </div>
             <div className="list-item">
               <span className="tag"></span>
-              <div className="title">Insturance</div>
+              <div className="title">List item 4</div>
             </div>
             <div className="list-item">
               <span className="tag"></span>
-              <div className="title">Estate planning</div>
+              <div className="title">List item 5</div>
             </div>
           </div>
         </div>
@@ -74,8 +58,8 @@ function App() {
           <div className="goals">
             <div className="goals-title">Goals</div>
             <div className="goals-list">
-              <div className="goals-list-item">Save $20,000 for a rainy day</div>
-              <div className="goals-list-item">Buy an inverstment property</div>
+              <div className="goals-list-item">Save $20,000 for a day</div>
+              <div className="goals-list-item">Buy an inverstment</div>
             </div>
             <div className="add-btn">Add another item</div>
             <div className="add-todo">Add a to-do</div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div className="pageList">
+    <a className="pageItem" href='/flex.html'>
+        flex
+    </a>
+    <a className="pageItem" href='/grid.html'>
+        grid
+    </a>
+</div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
