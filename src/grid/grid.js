63a7dfbed11920e8eee96ef56b270b9f963d22b1
ximan <ximan.wxm@gmail.com>
2019-06-18 15:45:05 +1000

    feat:

--- src/grid/grid.js
+++ src/grid/grid.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './grid.scss';
 
 function Grid () {
-
     return (
-        <div>grid</div>
+        <div className="container">
+            <div className="item item1">1</div>
+            <div className="item item2">2</div>
+            <div className="item item3">3</div>
+            <div className="item item4">4</div>
+            <div className="item item5">5</div>
+            <div className="item item6">6</div>
+            <div className="item item7">7</div>
+            <div className="item item8">8</div>
+            <div className="item item9">9</div>
+        </div>
     );
 }
 ReactDOM.render(<Grid />, document.getElementById('root'));
